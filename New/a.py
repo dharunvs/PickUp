f726c9a981b2f96e3e18b0ed984a311331b8cd1d
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2023-11-08 00:14:56 +0530

    updates

--- New/a.py
+++ New/a.py
@@ -1,5 +1,6 @@
-print('\n')
-print('Least Demand: E->A')
-print('Highest Demand: G->D')
-print('Current route: ["B", "F", "E", "A", "H"]')
-print('Suggested route: ["B", "F", "G", "D", "H"]')
+# print('\n')
+# print('Least Demand: E->A')
+# print('Highest Demand: G->D')
+# print('Current route: ["B", "F", "E", "A", "H"]')
+# print('Suggested route: ["B", "F", "G", "D", "H"]')
+
--- New/main.py
+++ New/main.py
@@ -72,74 +72,20 @@ def passengerFrequency():
     passenger_routes = loadJSON("passengerRoutes.json")
     passenger_routes = [f'{x["routes"]["legs"]["steps"][0]},{x["routes"]["legs"]["steps"][1]}' for x in passenger_routes]
     passenger_routes_distinct = list(set(passenger_routes))
-    # s = ""
-    # for i in passenger_routes_distinct:
-    #     s += f"{i.split(',')[0]}-{i.split(',')[1]},{passenger_routes.count(i)}\n"
-    # print(s)
+    s = ""
+    for i in passenger_routes_distinct:
+        s += f"{i.split(',')[0]}-{i.split(',')[1]},{passenger_routes.count(i)}\n"
+    print(s)
 
     passenger_frequency = sorted([[i.split(","), passenger_routes.count(i)] for i in passenger_routes_distinct], key = lambda x : -x[1])
-    # s = ""
-    # for i in passenger_frequency:
-    #     s += f"{i[0][0]}-{i[0][1]},{i[1]}\n"
-    # with open("passenger_frequency.csv", "w") as file:
-    #     file.write(s)
+    s = ""
+    for i in passenger_frequency:
+        s += f"{i[0][0]}-{i[0][1]},{i[1]}\n"
+    with open("passenger_frequency.csv", "w") as file:
+        file.write(s)
         
-    from sklearn.cluster import KMeans
-    from sklearn.preprocessing import OneHotEncoder
-    import pandas as pd
-    
-        # Sample data with a categorical variable 'Route_Type' for passengers and riders
-    passenger_data = pd.DataFrame({'Route_Type': ['D', 'F', 'D', 'E', 'F']})
-    rider_data = pd.DataFrame({'Route_Type': ['A', 'B', 'C', 'A', 'C']})
-
-    # Use one-hot encoding to transform the categorical variable into numeric columns
-    encoder = OneHotEncoder(sparse=False)
-
-    # Fit and transform the passenger and rider data
-    passenger_encoded = encoder.fit_transform(passenger_data[['Route_Type']])
-    rider_encoded = encoder.fit_transform(rider_data[['Route_Type']])
-
-    # Define the number of clusters (you can adjust this as needed)
-    num_clusters = 30
-
-    # Apply K-Means clustering to passenger and rider data
-    passenger_kmeans = KMeans(n_clusters=num_clusters)
-    rider_kmeans = KMeans(n_clusters=num_clusters)
-
-    passenger_clusters = passenger_kmeans.fit_predict(passenger_encoded)
-    rider_clusters = rider_kmeans.fit_predict(rider_encoded)
-    
-    print("-->", passenger_clusters)
-    print("-->", rider_clusters)
-
-    # Analyze the clusters and suggest alternate routes for riders as needed
-    # Your specific analysis would depend on the characteristics of your data and business logic
-
-    # Print the cluster assignments for passengers and riders
-    print("Passenger Clusters:", passenger_clusters)
-    print("Rider Clusters:", rider_clusters)
-    
-    data = pd.DataFrame({'Route_Type': ['D', 'F', 'D', 'E', 'F']})
-
-    # Use one-hot encoding to transform the categorical variable into numeric columns
-    encoder = OneHotEncoder(sparse=False)
-    encoded_data = encoder.fit_transform(data[['Route_Type']])
-    
-    print(data)
-    print(encoded_data)
-    
-    encoder = OneHotEncoder(sparse=False)
-    encoded_data = encoder.fit_transform(passenger_routes)
-    
-    kmeans = KMeans(n_clusters=len(passenger_routes_distinct))  # Choose an appropriate number of clusters
-    passenger_clusters = kmeans.fit_predict(encoded_data)
-    
-    print(passenger_clusters)
-   
-    # for i in passenger_routes_distinct:
-    #     passenger_frequency.append([i.split(","), passenger_routes.count(i)])
-
     return passenger_frequency
+
  
 def passengerFrequencyByClustering():
     from sklearn.cluster import KMeans
@@ -172,8 +118,106 @@ def passengerFrequencyByClustering():
         demand.append([combination, count])
         
     demand = sorted(demand, key= lambda x: -x[1])
-    
-    for i in demand:
-        print(i)
+        
+    return demand
 
-passengerFrequencyByClustering()
+def suggestRoute():
+    rider_routes = loadJSON("riderRoutes.json")
+    passenger_routes = loadJSON("passengerRoutes.json")
+
+    from ortools.constraint_solver import routing_enums_pb2
+    from ortools.constraint_solver import pywrapcp
+
+    # Extract the steps (locations) from rider routes
+    rider_steps = []
+    for route in rider_routes:
+        steps = route["routes"]["legs"]["steps"]
+        rider_steps.append(steps)
+
+    # Define the number of passengers and the maximum number of stops
+    num_passengers = len(passenger_routes)
+    max_stops = 5  # Adjust this based on your needs
+
+    # Create the data for the TSP with Time Windows (TSPTW)
+    data = {}
+    data['locations'] = rider_steps
+    data['num_vehicles'] = 1
+    data['depot'] = 0  # The depot is the starting and ending location (rider's home)
+
+    # Calculate the time windows for passengers based on their pickup and drop-off locations
+    time_windows = [(0, 0)]  # Rider starts from home, so no time window
+
+    # Generate time windows for passengers
+    for _ in range(num_passengers):
+        time_windows.append((0, max_stops))  # Adjust time windows as needed
+
+    data['time_windows'] = time_windows
+
+    # Create a function to calculate time between locations (you can customize this function)
+    def time_callback(from_node, to_node):
+        return 1  # In this example, we assume that travel time between locations is uniform
+
+    data['time_callback'] = time_callback
+
+    # Instantiate the routing solver
+    manager = pywrapcp.RoutingIndexManager(len(data['locations']), data['num_vehicles'], data['depot'])
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback
+    def time_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['time_callback'](from_node, to_node)
+
+    transit_callback_index = routing.RegisterTransitCallback(time_callback)
+
+    # Define cost of each arc (here, distance is set to 1)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Define the maximum number of stops
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.time_limit.seconds = 1
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+
+    # Solve the problem
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Get the route
+    if solution:
+        index = routing.Start(0)
+        route = []
+        while not routing.IsEnd(index):
+            route.append(manager.IndexToNode(index))
+            index = solution.Value(routing.NextVar(index))
+        
+        suggested_route = [data['locations'][node] for node in route]
+        
+
+        
+        for i in range(len(suggested_route)):
+            try:
+                suggested_route[i].remove(rider_routes[i]["routes"]["legs"]["steps"][0])
+            except:
+                pass
+            try:
+                suggested_route[i].remove(rider_routes[i]["routes"]["legs"]["steps"][-1])
+            except:
+                pass
+        
+        rider_routes = loadJSON("riderRoutes.json")
+        for i in range(len(suggested_route)):
+            # suggested_route[i] = list(set(suggested_route[i]))
+            suggested_route[i].insert(0, rider_routes[i]["routes"]["legs"]["steps"][0])
+            suggested_route[i].append(rider_routes[i]["routes"]["legs"]["steps"][-1])
+            
+        rider_routes = loadJSON("riderRoutes.json")
+        return rider_routes, suggested_route
+
+demand = passengerFrequencyByClustering()
+suggested_routes = suggestRoute()
+
+print(f'Least Demand: {demand[-1][0][0]}->{demand[-1][0][1]}')
+print(f'Highest Demand: {demand[0][0][0]}->{demand[0][0][1]}')
+print('Current route:', suggested_routes[0][1]["routes"]["legs"]["steps"])
+print('Suggested route:', suggested_routes[1][1])
